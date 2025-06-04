1058bf25f8235a08bb163247853a4efc2ec5b049
Sundaresan V <sundaresanv2004@gmail.com>
2025-06-04 13:23:45 +0530

    MLR

--- linear_model/LinearRegression.py
+++ linear_model/LinearRegression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class SingleLinearRegression:
+class SimpleLinearRegression:
     def __init__(self, learning_rate=0.01):
         self.weights = 0
         self.bias = 0
@@ -44,3 +44,54 @@ class SingleLinearRegression:
     @property
     def intercept_(self):
         return self.bias
+
+
+class MultiLinearRegression:
+    def __init__(self, learning_rate=1e-7):
+        self.weights = None
+        self.bias = 0
+        self.learning_rate = learning_rate
+        self.predicted_y = None
+        self.cost_history = []
+
+    def fit(self, x, y, epochs=100):
+        m, n = x.shape
+        self.weights = np.zeros(n)
+
+        for i in range(epochs):
+            self.predicted_y = np.dot(x, self.weights) + self.bias
+
+            loss = self.predicted_y - y
+            cost = (1 / (2 * m)) * np.sum(loss ** 2)
+            self.cost_history.append(cost)
+
+            dW = (1 / m) * np.dot(x.T, loss)
+            dB = np.mean(loss)
+
+            self.weights -= self.learning_rate * dW
+            self.bias -= self.learning_rate * dB
+
+            if i % 10 == 0:
+                print(f"Epoch {i}: Cost = {cost:.4f}")
+
+        return self.cost_history
+
+    def predict(self, x):
+        return np.dot(x, self.weights) + self.bias
+
+    @property
+    def coef_(self):
+        return self.weights
+
+    @property
+    def intercept_(self):
+        return self.bias
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01):
+        self.weight = 0
+        self.bias = 0
+        self.learning_rate = learning_rate
+
+    def fit(self, x, y, epochs=100):
+        ...
